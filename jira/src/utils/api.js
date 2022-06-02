8936cb5517d3890c33b0307657ef1e600671295a
Misavan <kh.van0618@gmail.com>
2022-06-03 00:26:03 +0700

    Update api.js

--- jira/src/utils/api.js
+++ jira/src/utils/api.js
@@ -6,7 +6,7 @@ api.interceptors.request.use((config)=>{
     config.headers = {
         TokenCybersoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAzMkUiLCJIZXRIYW5TdHJpbmciOiIxMC8wNC8yMDIzIiwiSGV0SGFuVGltZSI6IjE2ODEwODQ4MDAwMDAiLCJuYmYiOjE2NTA0NzQwMDAsImV4cCI6MTY4MTIzMjQwMH0._MPzAQorNhL5oVaR3-Az5_jYKW0_Acc0NBq1nZapr5k",
         Authorization: localStorage.getItem("JiraSignin")
-        ? `Bearer ${JSON.parse(localStorage.getItem("JiraLogin")).accessToken}`
+        ? `Bearer ${JSON.parse(localStorage.getItem("JiraLogin"))?.accessToken}`
         : ""
     };
     return config;
